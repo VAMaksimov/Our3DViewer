fadb10aee66e1bb5ebb851ab714caf3cf40ce5e1
VAMaksimov <154657436+VAMaksimov@users.noreply.github.com>
2025-06-06 11:12:58 +0000

    final stub

--- src/controller/desktop_app.cpp
+++ src/controller/desktop_app.cpp
@@ -3,7 +3,7 @@
 #include "view/viewer_widget.h"
 
 int main(int argc, char *argv[]) {
-  LogError("main", "\n-- Application started --");
+  s21::LogError("main", "\n\n-- Application started --");
   QApplication app(argc, argv);
   s21::ViewerWidget viewerWidget;
   viewerWidget.show();
